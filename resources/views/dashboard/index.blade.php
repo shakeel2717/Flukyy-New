38db395ddae124aec502b8273ebf12ae39300d26
shakeel2717 <shakeel2717@gmail.com>
2021-09-26 05:28:43 -0700

    Token withdraw to Convert

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -156,7 +156,7 @@
             <!-- Card -->
             <div class="card h-100">
                 <div class="card-body">
-                    <h6 class="card-subtitle mb-2">Total Token Withdraw</h6>
+                    <h6 class="card-subtitle mb-2">Total Token Convert</h6>
 
                     <div class="row align-items-center gx-2">
                         <div class="col">
